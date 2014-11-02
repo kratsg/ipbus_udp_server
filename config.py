217313be26c2680d0b164d42273c925224e7d43b
Giordon Stark <kratsg@gmail.com>
2014-11-02 16:18:16 -0600

    Updated the debugging output and cleaned up code a little bit. Started the infrastructure for registering callbacks to handle the various commands.

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import socket
 UDP_IP = "0.0.0.0"
 UDP_PORT = 8888
 
-FLAG_BIGENDIAN = None
+FLAG_LITTLE_ENDIAN = False
 REQUIRE_PROTOCOL_VERSION = '2'  # hex value of protocol version, in string
 WORD_LENGTH = 4  # in bytes
 
@@ -27,17 +26,53 @@ TRANSACTION_INFO_CODE_VALUES = {'0': 'request success',
 
 # packet header format comes in "keyName":(startIndex [from left], lengthOfValue)
 PACKET_HEADER_FORMAT = {"protocol_version": (0,1),
-                        "reserved": (1,1),
-                        "packet_id": (2,4),
-                        "byte-order_qualifier": (6,1),
-                        "packet_type": (7,1)}
+                        "reserved": (1,2),
+                        "packet_id": (2,6),
+                        "byte-order_qualifier": (6,7),
+                        "packet_type": (7,8)}
 
 TRANSACTION_HEADER_FORMAT = {"protocol_version": (0,1),
-                             "transaction_id": (1,3),
-                             "words": (4,2),
-                             "type_id": (6,1),
-                             "info_code": (7,1)}
+                             "transaction_id": (1,4),
+                             "words": (4,6),
+                             "type_id": (6,7),
+                             "info_code": (7,8)}
  
 # unicode for fancy outputting
 UNICODE = {'error': "\xE2\x9D\x8C ",
-           'warning': "\xE2\x9A\xA0\xEF\xB8\x8F "}
+           'warning': "\xE2\x9A\xA0\xEF\xB8\x8F ",
+           'leftrightarrow': "\xE2\x86\x94\xEF\xB8\x8F "}
+
+# for ease of formatting
+PRINT_LENGTH = 60  # number of columns
+
+
+class COLORS:
+  END = '\033[0m'
+
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+
+  HEADER = '\033[95m'
+  OKBLUE = BLUE
+  OKGREEN = GREEN
+  WARNING = YELLOW
+  FAIL = RED
+ 
+  @classmethod
+  def PRINT(cls):
+    for i, v in vars(cls).items():
+      if '__' in i:
+        continue
+      try:
+        print v+i+cls.ENDC
+      except:
+        continue
+
--- udp.py
+++ udp.py
@@ -1,21 +1,29 @@
+from __future__ import print_function
+
 import socket
+import struct
 from config import *
+import textwrap
 
-# unicode for fancy outputting
-UNICODE = {'error': "\xE2\x9D\x8C",
-           'warning': "\xE2\x9A\xA0\xEF\xB8\x8F"}
-
-def checkFalsey(booly):
+'''
+================================================
+                    HELPERS
+================================================
+'''
+def errStrOnFalsey(booly):
   global UNICODE
-  if booly:
-    return ""
-  else:
-    return UNICODE['error']
+  return "" if booly else UNICODE['error']
 
-def raiseEndianWarning():
-  global FLAG_BIGENDIAN, UNICODE
-  if FLAG_BIGENDIAN is True:
-    print "{} Warning: Big endian-ness detected!".format(UNICODE['warning'])
+def hexToBin(hex_val, numBits=8):
+  return ' '.join(chunks(bin(int(hex_val, 16))[2:].zfill(numBits), 8))
+
+# used by decodePacketHeader and decodeTransactionHeader 
+def reportHexVal(hex_val, flagError=True, parseFunction=None, numBits=8):
+  if parseFunction is None:
+    parseFunction_out = ''
+  else:
+    parseFunction_out = parseFunction(hex_val)
+  return "{}\t{}\t{:<16}\t{}".format(errStrOnFalsey(flagError), hex_val, hexToBin(hex_val, numBits=numBits), parseFunction_out)
 
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
@@ -23,71 +31,101 @@ def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
-# flip the word if globally required, can be overrided with `flip`
-def flipEndian(word, flipOverride=False):
-  global FLAG_BIGENDIAN, UNICODE
+def printBreakdown(**kwargs):
+    global PRINT_LENGTH, COLORS
+    outStr = []
+    dumpStr = "|{{:<{:d}}}|".format(PRINT_LENGTH-2)
 
-  if flipOverride:
-    print "{} Overriden: Flipping endian-ness of word".format(UNICODE['warning'])
+    words = kwargs.get('words', [])
+    indentLevel = kwargs.get('indentLevel', 0)  # default to 0
+    addr = kwargs.get('addr', ('127.0.0.1',8000))  # default address and port
+    inbound = kwargs.get('inbound', True)  # assume inbound
 
-  if FLAG_BIGENDIAN is None and not flipOverride:
-    ValueError('FLAG_BIGENDIAN is unset. Run checkEndian() on the packet header')
-    return word
-  elif FLAG_BIGENDIAN is True or flipOverride:
-    return ''.join(list(chunks(word, 2))[::-1])
-  else:
-    # do nothing, no flip needed
-    return word
-
-def decodePacketType(hex_val):
-  global PACKET_TYPE_VALUES
-  return PACKET_TYPE_VALUES.get(hex_val, 'reserved')
-
-def decodeTransactionType(hex_val):
-  global TRANSACTION_TYPE_VALUES
-  return TRANSACTION_TYPE_VALUES.get(hex_val, '?')
-
-def decodeTransactionInfoCode(hex_val):
-  global TRANSACTION_INFO_CODE_VALUES
-  return TRANSACTION_INFO_CODE_VALUES.get(hex_val, 'reserved')
+    outStr.append("="*PRINT_LENGTH)
+    outStr.append(dumpStr.format("{0:s}{1:s} Message{0:s}".format("-"*(PRINT_LENGTH/2 - 9), "Inbound " if inbound else "Outbound")))
+    # print inbound or outbound
+    outStr.append(dumpStr.format("    From: {:s}".format(addr)))
+    outStr.append(dumpStr.format("    Data"))
+    for i, word in enumerate(words):
+      outStr.append(dumpStr.format("     |- Word {:d}    {:s}".format(i, word)))
+    outStr.append(dumpStr.format("     ---------------------"))
+    outStr.append("="*PRINT_LENGTH)
+    print(("\t"*indentLevel)+("\n"+"\t"*indentLevel).join(outStr))
+    return True
 
 def printPacketDetails(packet, indentLevel=0):
-  print ("\t"*indentLevel)+("\n"+"\t"*indentLevel).join(packet['str'])
+  print(("\t"*indentLevel)+("\n"+"\t"*indentLevel).join(packet['str']))
   return True
 
 def printPacketsBreakdown(packets, indentLevel=0):
   outStr = []
   for i,v in enumerate(packets):
-    v = flipEndian(v)
     binNumber = ' '.join(chunks(bin(int(v, 16))[2:].zfill(32), 8))
     outStr.append("Word {:d} = {:s}".format(i, v))
     outStr.append("\t{:s}".format(binNumber))
-  print ("\t"*indentLevel)+("\n"+"\t"*indentLevel).join(outStr)
+  print(("\t"*indentLevel)+("\n"+"\t"*indentLevel).join(outStr))
   return True
 
+'''
+================================================
+                    ENDIANS
+================================================
+'''
+def raiseEndianWarning():
+  global FLAG_LITTLE_ENDIAN, UNICODE, PRINT_LENGTH
+  wrapper = textwrap.TextWrapper(subsequent_indent="{}\t".format(UNICODE['warning']), width=PRINT_LENGTH)
+  if FLAG_LITTLE_ENDIAN is True:
+    sentence = (COLORS.WARNING+"{} Warning: byte-order Little endian was detected! Will implicitly flip all words when decoding and flip back when encoding."+COLORS.END).format(UNICODE['warning'])
+    print(wrapper.fill(sentence))
+
 def checkEndian(packet_header):
-  global FLAG_BIGENDIAN
-  FLAG_BIGENDIAN = bool(packet_header[6:7] not in ['0','f'])
+  global FLAG_LITTLE_ENDIAN, PACKET_HEADER_FORMAT
+  start, end = PACKET_HEADER_FORMAT['byte-order_qualifier']
+  FLAG_LITTLE_ENDIAN = bool(packet_header[start:end] is not 'f')
   raiseEndianWarning()
 
-def hexToBin(hex_val, numBits=8):
-  return ' '.join(chunks(bin(int(hex_val, 16))[2:].zfill(numBits), 8))
- 
-# used by decodePacketHeader and decodeTransactionHeader 
-def reportHexVal(hex_val, flagError=True, parseFunction=None, numBits=8):
-  if parseFunction is None:
-    parseFunction_out = ''
+# flip the word if globally required, can be overrided with `flip`
+def flipEndian(word, flipOverride=False):
+  global FLAG_LITTLE_ENDIAN, UNICODE
+
+  if flipOverride:
+    print("{} Overriden: Flipping endian-ness of word".format(UNICODE['warning']))
+
+  if FLAG_LITTLE_ENDIAN is None and not flipOverride:
+    ValueError('FLAG_LITTLE_ENDIAN is unset. Run checkEndian() on the packet header')
+    return word
+  elif FLAG_LITTLE_ENDIAN is True or flipOverride:
+    return ''.join(list(chunks(word, 2))[::-1])
   else:
-    parseFunction_out = parseFunction(hex_val)
-  return "{}\t{}\t{}\t{}".format(checkFalsey(flagError), hex_val, hexToBin(hex_val, numBits=numBits), parseFunction_out)
+    # do nothing, no flip needed
+    return word
+
+'''
+================================================
+                    DECODERS
+================================================
+'''
+def decodePacketType(val):
+  global PACKET_TYPE_VALUES
+  hex_val = val['packet_type'] if 'packet_type' in val else val
+  return PACKET_TYPE_VALUES.get(hex_val, 'reserved')
+
+def decodeTransactionType(val):
+  global TRANSACTION_TYPE_VALUES
+  hex_val = val['type_id'] if 'type_id' in val else val
+  return TRANSACTION_TYPE_VALUES.get(hex_val, '?')
+
+def decodeTransactionInfoCode(val):
+  global TRANSACTION_INFO_CODE_VALUES
+  hex_val = val['info_code'] if 'info_code' in val else val
+  return TRANSACTION_INFO_CODE_VALUES.get(hex_val, 'reserved')
 
 def decodePacketHeader(word):
     global REQUIRE_PROTOCOL_VERSION, PACKET_HEADER_FORMAT
-    checkEndian(word)
     word = flipEndian(word)
     parsedWord = {}
     for k,v in PACKET_HEADER_FORMAT.iteritems():
-      parsedWord[k] = word[v[0]:v[0]+v[1]]
+      parsedWord[k] = word[v[0]:v[1]]
 
     outStr = []
     outStr.append("(4b)  Protocol Version")
@@ -108,7 +146,7 @@ def decodeTransactionHeader(word, checkOutboundRequest=True):
     word = flipEndian(word)
     parsedWord = {}
     for k,v in TRANSACTION_HEADER_FORMAT.iteritems():
-      parsedWord[k] = word[v[0]:v[0]+v[1]]
+      parsedWord[k] = word[v[0]:v[1]]
 
     outStr = []
     outStr.append("(4b)  Protocol Version")
@@ -124,46 +162,85 @@ def decodeTransactionHeader(word, checkOutboundRequest=True):
     parsedWord['str'] = outStr
     return parsedWord
 
-def encodePacket(PACKET_FORMAT, packet, **kwargs):
-  global WORD_LENGTH
-  outbound_packet = ['']*WORD_LENGTH*2  # put in hex
-  for k,v in PACKET_FORMAT.iteritems():
-    outbound_packet[v[0]:v[0]+v[1]] = kwargs.get(k, packet[k])
-  return flipEndian(''.join(outbound_packet), flipOverride=True)
+'''
+================================================
+                    ENCODERS
+================================================
+'''
 
+def encodeWord(WORD_FORMAT, **kwargs):
+  ''' Given a WORD_FORMAT (such as PACKET_HEADER_FORMAT)
+       return the word in appropriate format (little or big endian)
+       based on the format defined with the ordering
+   - Precendence of setting value in a word is by:
+         - kwargs['key']
+         - word['key']
+         - '0' (default!)
+    If no kwargs are set other than kwargs['word'], then a copy of kwargs['word'] is used.
+  '''
+
+  global WORD_LENGTH
+  word = kwargs.get('word', {})
+  outbound_word = ['']*WORD_LENGTH*2  # put in hex
+  for k,v in WORD_FORMAT.iteritems():
+    outbound_word[v[0]:v[1]] = kwargs.get(k, word.get(k, '0'))
+  return flipEndian(''.join(outbound_word))
+
+'''
+================================================
+                 ACTUAL SCRIPT
+================================================
+'''
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+def controlHandler(words):
+  parsed_transactionData = decodeTransactionHeader(words[0])
+  print("Transaction Details")
+  printPacketDetails(parsed_transactionData, indentLevel=1)
+ 
+  print("Responding to Control Request")
+  responsePackets = []
+  responsePackets.append(encodeWord(TRANSACTION_HEADER_FORMAT, word=parsed_transactionData, info_code='0'))
+  return responsePackets
+ 
+def statusHandler(words):
+  pass
+
+def resendHandler(words):
+  pass
+
+packetTypeHandler = {'control (r/w)': controlHandler,
+                     'status': statusHandler,
+                     're-send': resendHandler}
+
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    # listen for data, buffer size is 1024 bytes
+    data, addr = sock.recvfrom(1024)
+    # encode it in hex
     data = data.encode("hex")
-    print "received message:", data, addr
     words = list(chunks(data,8))
+    printBreakdown(words=words)
 
-    parsed_packetData = decodePacketHeader(words[0])
-    print "Packet Details"
-    printPacketDetails(parsed_packetData, indentLevel=1)
+    # Step 0: determine endian-ness using packet header
+    checkEndian(words[0])
+    
+    packetHeader = decodePacketHeader(words[0])
+    print("Packet Header")
+    printPacketDetails(packetHeader, indentLevel=1)
 
-    parsed_transactionData = decodeTransactionHeader(words[1])
-    print "Transaction Details"
-    printPacketDetails(parsed_transactionData, indentLevel=1)
+    packetType = decodePacketType(packetHeader)
+    print("Handling Packet Type:", packetType)
+    handler = packetTypeHandler.get(packetType, lambda: print("ValueError: unknown type"))
+    responsePackets = handler(words[1:])  # return a list of response words
 
-    print "Packets Breakdown"
-    printPacketsBreakdown(words, indentLevel=1)
+    # printPacketsBreakdown(words, indentLevel=1)
 
-    print "Responding with Transaction Header"
-    responsePackets = []
-    responsePackets.append(encodePacket(PACKET_HEADER_FORMAT, parsed_packetData))
-    responsePackets.append(encodePacket(TRANSACTION_HEADER_FORMAT, parsed_transactionData, info_code='0'))
-    print "Checking"
-    printPacketDetails(decodePacketHeader(responsePackets[0]))
-    printPacketDetails(decodeTransactionHeader(responsePackets[1], checkOutboundRequest=False))
-    printPacketsBreakdown(responsePackets, indentLevel=1)
+    responsePackets.insert(0, encodeWord(PACKET_HEADER_FORMAT, word=packetHeader))
+    printBreakdown(words=responsePackets, inbound=False, addr=(UDP_IP, UDP_PORT))
 
-    print "Sending"
+    print("Sending", end="")
     sock.sendto((''.join(responsePackets)).decode("hex"), addr)
-
-    print "ENDING"
-    print "="*60
+    print("... sent!")
 
